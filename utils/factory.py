481ce0d8dde72fe7bc6c19ab23b8e8cf99d4885d
Ege Erdal <ege-erdal101@hotmail.com>
2026-05-01 22:51:43 +0200

    Fix conditional logic in get_model function to use 'elif' for adaptive_gating_tosca

--- utils/factory.py
+++ utils/factory.py
@@ -2,7 +2,7 @@ def get_model(model_name, args):
     name = model_name.lower()
     if name == 'tosca':
         from models.tosca import Learner
-    if name == 'adaptive_gating_tosca':
+    elif name == 'adaptive_gating_tosca':
         from models.adaptive_gating_tosca import Learner
     else:
         raise NotImplementedError("Unknown model: {}".format(model_name))
