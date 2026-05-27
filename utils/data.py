20cba69c10a543a9f80a97f8240cb55b7a9f8b15
Ege Erdal <ege-erdal101@hotmail.com>
2026-05-27 20:31:37 +0200

    Wire up ImageNet-R dataset + adapter-gating INR config

--- utils/data.py
+++ utils/data.py
@@ -224,9 +224,8 @@ class iImageNetR(iData):
         self.class_order = np.arange(200).tolist()
 
     def download_data(self):
-        assert 0, "You should specify the folder of your dataset"
-        train_dir = "[DATA-PATH]/train/"
-        test_dir = "[DATA-PATH]/test/"
+        train_dir = "data/imagenet-r-split/train/"
+        test_dir = "data/imagenet-r-split/test/"
 
         train_dset = datasets.ImageFolder(train_dir)
         test_dset = datasets.ImageFolder(test_dir)
